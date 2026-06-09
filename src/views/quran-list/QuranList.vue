1033505be9b2c1e8041e5b0cf935d2a26b48aebf
syifa sukma <syifasukmaramadhani@gmail.com>
2026-06-09 14:01:45 +0700

    feature: add controller audio at detail surah

--- src/views/quran-list/QuranList.vue
+++ src/views/quran-list/QuranList.vue
@@ -1,10 +1,10 @@
 <script setup>
-import { useSurahStore } from '@/stores/surahs';
-import { computed, onMounted, watch } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
+import { useSurahStore } from "@/stores/surahs";
+import { computed, onMounted, watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
 
 const props = defineProps({
-  search: String,
+    search: String,
 });
 
 const route = useRoute();
@@ -12,77 +12,72 @@ const router = useRouter();
 const surahStore = useSurahStore();
 
 onMounted(() => {
-  if (!surahStore.surahs.length) {
-    surahStore.fetchSurahs();
-  }
+    if (!surahStore.surahs.length) {
+        surahStore.fetchSurahs();
+    }
 });
 
 const filteredSurah = computed(() => {
-  return surahStore.surahs.filter((surah) =>
-    surah.namaLatin.toLowerCase().includes((props.search || '').toLowerCase()),
-  );
+    return surahStore.surahs.filter((surah) =>
+        surah.namaLatin.toLowerCase().includes((props.search || "").toLowerCase()),
+    );
 });
 
 const goToDetail = (id) => {
-  router.push(`/quran/surah/${id}`);
+    router.push(`/quran/surah/${id}`);
 };
 
 watch(
-  () => route.fullPath,
-  () => {
-    surahStore.fetchSurahs();
-  },
+    () => route.fullPath,
+    () => {
+        surahStore.fetchSurahs();
+    },
 );
 </script>
 
 <template>
-
     <div v-if="filteredSurah.length === 0" class="text-center py-16">
-    <i class="pi pi-search text-5xl text-primary mb-4"></i>
+        <i class="pi pi-search text-5xl text-primary mb-4"></i>
 
-    <h1 class="text-xl font-semibold text-primary">
-      Pencarian Quran Tidak Ditemukan
-    </h1>
+        <h1 class="text-xl font-semibold text-primary">Pencarian Quran Tidak Ditemukan</h1>
 
-    <p class="text-gray-400 mt-2">
-      Coba gunakan kata kunci lain
-    </p>
-  </div>
-
-  <div v-else class="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-    <div
-      v-for="(surah, index) in filteredSurah"
-      :key="surah.nomor"
-      v-animate="{ type: 'fadeUp', delay: index * 2 }"
-      @click="goToDetail(surah.nomor)"
-      class="bg-white rounded-xl dark:bg-muted/10 cursor-pointer px-4 py-4 hover:border-t-2 hover:border-primary shadow-md hover:shadow-xl transition-[transform,box-shadow] duration-300 ease-out hover:-translate-y-1 hover:-translate-x-0.5"
-    >
-      <div class="flex items-center justify-between gap-3">
-        <div class="flex items-center gap-3">
-          <div
-            class="rounded-full bg-primary/10 px-4 py-4 border border-primary text-primary font-bold flex items-center justify-center w-[24px] h-[24px]"
-          >
-            {{ surah.nomor }}
-          </div>
-
-          <div class="space-y-2">
-            <div class="flex gap-2 items-center">
-              <h1 class="font-semibold dark:text-white">{{ surah.namaLatin }}</h1>
-              <span class="text-sm text-primary">({{ surah.arti }})</span>
-            </div>
-
-            <div class="flex gap-2 mt-1 text-sm text-gray-500">
-              <span>{{ surah.jumlahAyat }} Ayat</span>
-              <span>|</span>
-              <span>{{ surah.tempatTurun }}</span>
-            </div>
-          </div>
-        </div>
-
-        <div class="text-lg font-bold text-primary">
-          <p>{{ surah.nama }}</p>
-        </div>
-      </div>
+        <p class="text-gray-400 mt-2">Coba gunakan kata kunci lain</p>
     </div>
-  </div>
+
+    <div v-else class="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div
+            v-for="(surah, index) in filteredSurah"
+            :key="surah.nomor"
+            v-animate="{ type: 'fadeUp', delay: index * 2 }"
+            @click="goToDetail(surah.nomor)"
+            class="bg-white rounded-xl dark:bg-muted/10 cursor-pointer px-4 py-4 hover:border-t-2 hover:border-primary shadow-md hover:shadow-xl transition-[transform,box-shadow] duration-300 ease-out hover:-translate-y-1 hover:-translate-x-0.5"
+        >
+            <div class="flex items-center justify-between gap-3">
+                <div class="flex items-center gap-3">
+                    <div
+                        class="rounded-full bg-primary/10 px-4 py-4 border border-primary text-primary font-bold flex items-center justify-center w-[24px] h-[24px]"
+                    >
+                        {{ surah.nomor }}
+                    </div>
+
+                    <div class="space-y-2">
+                        <div class="flex gap-2 items-center">
+                            <h1 class="font-semibold dark:text-white">{{ surah.namaLatin }}</h1>
+                            <span class="text-sm text-primary">({{ surah.arti }})</span>
+                        </div>
+
+                        <div class="flex gap-2 mt-1 text-sm text-gray-500">
+                            <span>{{ surah.jumlahAyat }} Ayat</span>
+                            <span>|</span>
+                            <span>{{ surah.tempatTurun }}</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="text-lg font-bold text-primary">
+                    <p>{{ surah.nama }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
--- src/views/quran-list/detail/QuranDetail.vue
+++ src/views/quran-list/detail/QuranDetail.vue
@@ -21,6 +21,9 @@ const bookmarks = ref([]);
 const isLoading = ref(true);
 const errorMessage = ref("");
 const themeStore = useThemeStore();
+const currentAyat = ref(null);
+const currentAudio = ref(null);
+const isPlaying = ref(false);
 
 const handleClickOutsideAll = (e) => {
     if (dropdownRef.value && !dropdownRef.value.contains(e.target)) {
@@ -90,7 +93,7 @@ watch(
 
 const audio = new Audio();
 
-const playAudio = async (url) => {
+const playAudio = async (url, ayatNumber) => {
     try {
         audio.pause();
         audio.currentTime = 0;
@@ -98,16 +101,43 @@ const playAudio = async (url) => {
         audio.src = url;
 
         await audio.play();
+
+        currentAudio.value = url;
+        currentAyat.value = ayatNumber;
+        isPlaying.value = true;
     } catch (error) {
+        console.error(error);
         alert("Audio gagal diputar");
     }
 };
 const stopAudio = () => {
     audio.pause();
     audio.currentTime = 0;
+
+    currentAudio.value = null;
+    currentAyat.value = null;
+    isPlaying.value = false;
 };
 
+const pauseAudio = () => {
+    audio.pause();
+    isPlaying.value = false;
+};
+
+const resumeAudio = async () => {
+    try {
+        await audio.play();
+        isPlaying.value = true;
+    } catch (error) {
+        console.error(error);
+    }
+};
 onMounted(() => {
+    audio.addEventListener("ended", () => {
+        currentAudio.value = null;
+        isPlaying.value = false;
+    });
+
     window.addEventListener("beforeunload", stopAudio);
 
     document.addEventListener("visibilitychange", () => {
@@ -127,10 +157,15 @@ onMounted(() => {
 });
 
 const toggleBookmark = (ayat) => {
-    const isExist = bookmarks.value.find((item) => item.surahId === Number(route.params.id) && item.ayatNumber === ayat.nomorAyat);
+    const isExist = bookmarks.value.find(
+        (item) => item.surahId === Number(route.params.id) && item.ayatNumber === ayat.nomorAyat,
+    );
 
     if (isExist) {
-        bookmarks.value = bookmarks.value.filter((item) => !(item.surahId === Number(route.params.id) && item.ayatNumber === ayat.nomorAyat));
+        bookmarks.value = bookmarks.value.filter(
+            (item) =>
+                !(item.surahId === Number(route.params.id) && item.ayatNumber === ayat.nomorAyat),
+        );
     } else {
         bookmarks.value.push({
             surahId: Number(route.params.id),
@@ -145,11 +180,13 @@ const toggleBookmark = (ayat) => {
 };
 
 const isBookmarked = (ayat) => {
-    return bookmarks.value.some((item) => item.surahId === Number(route.params.id) && item.ayatNumber === ayat.nomorAyat);
+    return bookmarks.value.some(
+        (item) => item.surahId === Number(route.params.id) && item.ayatNumber === ayat.nomorAyat,
+    );
 };
 
 const shareToWhatsapp = (ayat) => {
-    const text = `📖 ${surahStore.surahDetail.namaLatin} - Ayat ${ayat.nomorAyat} ${ayat.teksArab} ${ayat.teksIndonesia} ${window.location.href}#ayat-${ayat.nomorAyat}`;
+    const text = `${surahStore.surahDetail.namaLatin} - Ayat ${ayat.nomorAyat} ${ayat.teksArab} ${ayat.teksIndonesia} ${window.location.href}#ayat-${ayat.nomorAyat}`;
 
     const url = `https://wa.me/?text=${encodeURIComponent(text)}`;
 
@@ -167,12 +204,21 @@ const shareToWhatsapp = (ayat) => {
             {{ errorMessage }}
         </p>
 
-        <button @click="router.go(0)" class="mt-5 bg-primary text-white px-5 py-2 rounded-xl cursor-pointer">Coba Lagi</button>
+        <button
+            @click="router.go(0)"
+            class="mt-5 bg-primary text-white px-5 py-2 rounded-xl cursor-pointer"
+        >
+            Coba Lagi
+        </button>
     </div>
 
     <div v-else class="relative">
         <div class="h-[80px] bg-primary w-full overflow-hidden relative z-0">
-            <img src="../../../assets/alquranwithoutbg.png" class="absolute inset-0 w-full h-full object-cover opacity-30 scale-110" alt="alquran without background" />
+            <img
+                src="../../../assets/alquranwithoutbg.png"
+                class="absolute inset-0 w-full h-full object-cover opacity-30 scale-110"
+                alt="alquran without background"
+            />
 
             <div class="absolute inset-0 bg-gradient-to-b from-primary/80 to-primary/60"></div>
         </div>
@@ -180,14 +226,19 @@ const shareToWhatsapp = (ayat) => {
         <div class="relative -mt-[75px] z-10 px-4 lg:px-28">
             <div class="bg-transparent rounded-t-3xl px-4 pt-4">
                 <div class="flex justify-between items-center mb-6">
-                    <div @click="router.push('/quran/surah')" class="flex items-center gap-2 cursor-pointer">
+                    <div
+                        @click="router.push('/quran/surah')"
+                        class="flex items-center gap-2 cursor-pointer"
+                    >
                         <i class="pi pi-arrow-left text-white text-lg cursor-pointer"></i>
                         <p class="text-white font-semibold text-base">Daftar Surah</p>
                     </div>
 
                     <div class="flex items-center gap-3">
                         <p class="text-white font-semibold">{{ name }}</p>
-                        <div class="rounded-full bg-primary/10 px-3 py-2.5 border border-white text-white font-bold">
+                        <div
+                            class="rounded-full bg-primary/10 px-3 py-2.5 border border-white text-white font-bold"
+                        >
                             {{ name.slice(0, 2).toUpperCase() }}
                         </div>
                         <button
@@ -195,7 +246,11 @@ const shareToWhatsapp = (ayat) => {
                             aria-label="button dark mode"
                             class="cursor-pointer inline-flex items-center gap-2 p-4 rounded-full text-lg font-semibold bg-white text-gray-600 dark:text-white dark:bg-muted transition-all duration-200"
                         >
-                            <i :class="themeStore.isDark ? 'pi pi-moon text-lg' : 'pi pi-sun text-lg'" />
+                            <i
+                                :class="
+                                    themeStore.isDark ? 'pi pi-moon text-lg' : 'pi pi-sun text-lg'
+                                "
+                            />
                         </button>
                     </div>
                 </div>
@@ -204,15 +259,23 @@ const shareToWhatsapp = (ayat) => {
 
         <div class="-mt-5">
             <div class="flex items-center flex-col">
-                <div class="bg-gray-400/20 w-full text-center flex flex-col items-center justify-start lg:justify-center py-8">
-                    <h1 class="text-2xl font-bold dark:text-primary">{{ surahStore.surahDetail.nama }}</h1>
+                <div
+                    class="bg-gray-400/20 w-full text-center flex flex-col items-center justify-start lg:justify-center py-8"
+                >
+                    <h1 class="text-2xl font-bold dark:text-primary">
+                        {{ surahStore.surahDetail.nama }}
+                    </h1>
                     <div class="flex gap-2 items-center mt-1">
-                        <p class="font-semibold dark:text-white">{{ surahStore?.surahDetail?.namaLatin }}</p>
+                        <p class="font-semibold dark:text-white">
+                            {{ surahStore?.surahDetail?.namaLatin }}
+                        </p>
                         <p class="text-sm dark:text-primary">({{ surahStore.surahDetail.arti }})</p>
                     </div>
 
                     <div class="flex gap-2 items-center mt-2">
-                        <p class="border border-gray-500 px-3 py-1 rounded-lg text-gray-400"><i class="pi pi-book"></i> {{ surahStore.surahDetail.jumlahAyat }} Ayat</p>
+                        <p class="border border-gray-500 px-3 py-1 rounded-lg text-gray-400">
+                            <i class="pi pi-book"></i> {{ surahStore.surahDetail.jumlahAyat }} Ayat
+                        </p>
                         <div class="w-1 h-1 bg-gray-400 rounded-full"></div>
                         <p class="border border-gray-500 px-3 py-1 rounded-lg text-gray-400">
                             <i class="pi pi-file"></i>
@@ -226,7 +289,8 @@ const shareToWhatsapp = (ayat) => {
                                 @click="isOpen = !isOpen"
                                 class="border border-gray-400/40 hover:bg-gray-100 hover:text-primary text-black px-4 py-2 rounded-lg flex items-center justify-between gap-2 w-full cursor-pointer dark:bg-muted dark:border-white dark:text-white"
                             >
-                                Surah <i class="pi pi-chevron-down text-gray-400 dark:text-white"></i>
+                                Surah
+                                <i class="pi pi-chevron-down text-gray-400 dark:text-white"></i>
                             </button>
 
                             <div
@@ -250,14 +314,20 @@ const shareToWhatsapp = (ayat) => {
                                 @click="isOpenAyat = !isOpenAyat"
                                 class="border border-gray-400/40 hover:bg-gray-100 hover:text-primary text-black px-4 py-2 rounded-lg flex items-center justify-between gap-2 w-full cursor-pointer dark:bg-muted dark:border-white dark:text-white"
                             >
-                                Ayat <i class="pi pi-chevron-down text-gray-400 dark:text-white"></i>
+                                Ayat
+                                <i class="pi pi-chevron-down text-gray-400 dark:text-white"></i>
                             </button>
 
                             <div
                                 v-if="isOpenAyat"
                                 class="absolute mt-2 w-40 cursor-pointer bg-white border rounded-lg shadow-lg z-50 h-48 overflow-auto hover:bg-gray-100 hover:text-primary dark:bg-muted dark:border-white dark:text-white"
                             >
-                                <div v-for="ayat in surahStore.surahDetail.ayat" :key="ayat.nomorAyat" @click="scrollToAyat(ayat.nomorAyat)" class="py-2">
+                                <div
+                                    v-for="ayat in surahStore.surahDetail.ayat"
+                                    :key="ayat.nomorAyat"
+                                    @click="scrollToAyat(ayat.nomorAyat)"
+                                    class="py-2"
+                                >
                                     {{ ayat.nomorAyat }}
                                 </div>
                             </div>
@@ -266,20 +336,41 @@ const shareToWhatsapp = (ayat) => {
                 </div>
 
                 <div class="px-4 md:pr-8 md:pl-12 lg:px-28">
-                    <h1 class="text-center font-semibold my-8 text-2xl dark:text-primary">بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ</h1>
-                    <div v-for="ayat in surahStore.surahDetail.ayat" :key="ayat.ayat" class="py-2" :id="`ayat-${ayat.nomorAyat}`">
+                    <h1 class="text-center font-semibold my-8 text-2xl dark:text-primary">
+                        بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ
+                    </h1>
+                    <div
+                        v-for="ayat in surahStore.surahDetail.ayat"
+                        :key="ayat.ayat"
+                        class="py-2"
+                        :id="`ayat-${ayat.nomorAyat}`"
+                    >
                         <p class="text-right text-2xl font-semibold dark:text-primary">
                             {{ ayat.teksArab }}
                         </p>
-                        <p class="text-left text-primary font-semibold mt-5 text-lg">{{ ayat.teksLatin }}</p>
-                        <p class="text-left mt-3 text-lg dark:text-white">{{ ayat?.nomorAyat }}. {{ ayat.teksIndonesia }}</p>
+                        <p class="text-left text-primary font-semibold mt-5 text-lg">
+                            {{ ayat.teksLatin }}
+                        </p>
+                        <p class="text-left mt-3 text-lg dark:text-white">
+                            {{ ayat?.nomorAyat }}. {{ ayat.teksIndonesia }}
+                        </p>
 
                         <div class="flex items-center gap-3 mt-4">
-                            <i class="pi pi-play-circle text-primary text-3xl cursor-pointer" @click="playAudio(ayat.audio?.['01'])"></i>
-                            <i class="pi pi-share-alt text-primary text-2xl cursor-pointer" @click="shareToWhatsapp(ayat)"></i>
+                            <i
+                                class="pi pi-play-circle text-primary text-3xl cursor-pointer"
+                                @click="playAudio(ayat.audio?.['01'], ayat.nomorAyat)"
+                            ></i>
+                            <i
+                                class="pi pi-share-alt text-primary text-2xl cursor-pointer"
+                                @click="shareToWhatsapp(ayat)"
+                            ></i>
                             <i
                                 class="pi text-2xl cursor-pointer"
-                                :class="isBookmarked(ayat) ? 'pi-bookmark-fill text-yellow-500' : 'pi-bookmark text-primary'"
+                                :class="
+                                    isBookmarked(ayat)
+                                        ? 'pi-bookmark-fill text-yellow-500'
+                                        : 'pi-bookmark text-primary'
+                                "
                                 @click="toggleBookmark(ayat)"
                             ></i>
                         </div>
@@ -289,4 +380,57 @@ const shareToWhatsapp = (ayat) => {
             </div>
         </div>
     </div>
+
+    <Transition
+        enter-active-class="transition duration-300"
+        enter-from-class="translate-y-full opacity-0"
+        enter-to-class="translate-y-0 opacity-100"
+        leave-active-class="transition duration-300"
+        leave-from-class="translate-y-0 opacity-100"
+        leave-to-class="translate-y-full opacity-0"
+    >
+        <div
+            class="fixed bottom-4 left-1/2 -translate-x-1/2 bg-white dark:bg-white/80 shadow-xl rounded-2xl p-4 z-[999] border border-gray-300x md:w-1/3 w-90 dark:border-white dark:backdropbackdrop-blur-2xl"
+            v-if="currentAudio"
+        >
+            <div class="flex flex-col items-center justify-between">
+                <div>
+                    <p class="font-semibold dark:text-black">Sedang Memutar Audio</p>
+
+                    <div class="flex items-center justify-center gap-2">
+                        <p class="text-sm text-gray-500 dark:text-black">
+                            {{ surahStore.surahDetail.namaLatin }}
+                        </p>
+                        <span class="text-gray-500 dark:text-black">-</span>
+                        <p class="text-sm text-gray-500 dark:text-black">Ayat {{ currentAyat }}</p>
+                    </div>
+                </div>
+
+                <div class="flex items-center gap-3 mt-4">
+                    <button
+                        v-if="isPlaying"
+                        @click="pauseAudio"
+                        class="w-10 h-10 rounded-full bg-yellow-500 text-white flex items-center justify-center cursor-pointer"
+                    >
+                        <i class="pi pi-pause"></i>
+                    </button>
+
+                    <button
+                        v-else
+                        @click="resumeAudio"
+                        class="w-10 h-10 rounded-full bg-primary text-white flex items-center justify-center cursor-pointer"
+                    >
+                        <i class="pi pi-play"></i>
+                    </button>
+
+                    <button
+                        @click="stopAudio"
+                        class="w-10 h-10 rounded-full bg-red-800 text-white flex items-center justify-center cursor-pointer"
+                    >
+                        <i class="pi pi-stop"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </Transition>
 </template>
